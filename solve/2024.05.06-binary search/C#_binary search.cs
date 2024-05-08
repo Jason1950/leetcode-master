b83234380a3df8ec131a7c290e4871abfb4a784f
henry.shen <henry.shen@onelab.tw>
2024-05-08 11:31:29 +0800

    udpate: correct binary search

--- solve/2024.05.06-binary search/C#_binary search.cs
+++ solve/2024.05.06-binary search/C#_binary search.cs
@@ -1,43 +1,43 @@
-//Microsoft (R) Visual C# Compiler version 3.4.0-beta4-19562-05 (ff930dec)
-//Copyright (C) Microsoft Corporation. All rights reserved.
+// Example 1:
+//
+// Input: nums = [-1,0,3,5,9,12], target = 9
+// Output: 4
+// Explanation: 9 exists in nums and its index is 4
+// Example 2:
+//
+// Input: nums = [-1,0,3,5,9,12], target = 2
+// Output: -1
+// Explanation: 2 does not exist in nums so return -1
 
+// O(log n)
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text.RegularExpressions;
+// 注意 l,h 的更新方式 及初始化 h= length-1
 
-namespace Rextester
-{
-    public class Program
+var solve = new Solution();
+var ans =solve.Search(new int[] { -1, 0,5 },5);
+Console.WriteLine("ans: "+ans);
+
+public class Solution {
+    public int Search(int[] nums, int target)
     {
-        public static void Main(string[] args)
-        {
-            //Your code goes here
-            Console.WriteLine("Hello, world!");
-            var a = new[]{            -1,0,3,5,9,12};
-            Search(a,9);
-        }
-        
-        public static int Search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.Length-1;
-        while(left<=right){
-            int middle = left+((right+left)/2);
-            Console.WriteLine($"middle {middle} ,left {left} ,right {right}");
+        var (l, h) = (0,nums.Length-1);
 
-            if(nums[middle]>target){
-                Console.WriteLine($"cut right: {right} , middle-1 ->{middle-1}");
-                right=middle-1;
-            }else if(nums[middle]<target){
-                Console.WriteLine($"add left: {left} , middle+1 ->{middle+1}");
-                left= middle+1;
-            }
-            else{
+        while (l<=h)
+        {
+            var middle = (int)Math.Floor((l+h) / 2.0);;
+            Console.WriteLine($"l {l}, h {h}, m {middle}");
+            if (nums[middle]==target)
+            {
                 return middle;
+            }else if (nums[middle]>target)
+            {
+                h = middle - 1;
+            }
+            else
+            {
+                l = middle + 1;
             }
         }
         return -1;
-    }
     }
 }
